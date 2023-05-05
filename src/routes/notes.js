9eb9670e67044940c15098dace53bdbded02e4f8
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-05-05 18:42:21 +0530

    test users

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,9 +2,14 @@ const express = require('express');
 const validator = require('../middleware/validator');
 const controller = require('../controllers/notes');
 const router = express.Router();
+const model = require('../models/notes');
 
 router.post('/', validator, controller.createNote);
 router.get('/', validator, controller.getNotes);
+router.get('/test', async (req, res) => {
+    const notes = await model.find({})
+    res.status(200).json(notes);
+});
 router.get('/:id', validator, controller.getANote);
 router.put('/:id', validator, controller.updateNote);
 router.delete('/:id', validator, controller.deleteNote)
